5cc2aa95990bb4d80363bc47ec7aac0e7813f9ea
tommy-cash <mikestomas.99@gmail.com>
2025-05-06 23:14:23 +0200

    Implement product aggregate and related value objects

---
+++ MikesEshop.Products/Core/Product.cs
@@ -0,0 +1,52 @@
+using Ardalis.GuardClauses;
+using MikesEshop.Products.Core.ValueObjects;
+using MikesEshop.Shared.Core;
+
+namespace MikesEshop.Products.Core;
+
+public class Product : EntityBase
+{
+    public string Name { get; private set; }
+    public string ImageUrl { get; private set; }
+    public string? Description { get; private set; }
+    public int? StockedQuantity { get; private set; }
+
+    public Price? Price { get; private set; }
+    public Dimensions? Dimensions { get; private set; }
+    
+    private Product() { }
+     
+    public Product(Guid id,
+        string name,
+        string imageUrl,
+        string? description = null,
+        int? stockedQuantity = null,
+        Price? price = null,
+        Dimensions? dimensions = null)
+    {
+        Id = id;
+        Name = name;
+        ImageUrl = imageUrl;
+        Description = description;
+        Price = price;
+        StockedQuantity = stockedQuantity;
+        Dimensions = dimensions;
+    }
+
+    public void UpdateStock(int quantity)
+    {
+        Guard.Against.Negative(quantity, nameof(quantity), "Quantity of stock cannot be negative");
+        
+        StockedQuantity = quantity;
+    }
+    
+    public void UpdatePrice(decimal newAmount, string newCurrency)
+    {
+        Price = new Price(newAmount, newCurrency);
+    }
+    
+    public void UpdateDimensions(decimal newWidth, decimal newHeight, decimal newDepth)
+    {
+        Dimensions = new Dimensions(newWidth, newHeight, newDepth);
+    }
+}
